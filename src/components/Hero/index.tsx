abffbfbc853bf188b1b38b206c3272a3260a7790
Maclevison Silva <maclevison@gmail.com>
2021-05-09 16:07:58 +0100

    chore: hero component update

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,5 +1,21 @@
 import * as S from './styles'
 
-const Hero = () => <S.Hero>Hello Routines</S.Hero>
+const Hero = () => (
+    <S.Hero>
+        <S.LeftCol>
+            <div>
+                o que criativos <br /> fazem <em>diariamente?</em>
+            </div>
+            <div>
+                a rotina diária de profissionais Para inspirar a sua e deixa-la
+                ainda mais produtiva.
+            </div>
+        </S.LeftCol>
+        <S.RightCol>
+            <img src="/images/text-circle.svg" alt="" />
+            <S.HeroImage src="/images/hero-circle.png" alt="" />
+        </S.RightCol>
+    </S.Hero>
+)
 
 export default Hero
--- src/components/Hero/styles.ts
+++ src/components/Hero/styles.ts
@@ -1,3 +1,36 @@
 import styled from 'styled-components'
 
-export const Hero = styled.div``
+export const Hero = styled.div`
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-column-gap: 0;
+    height: 85vh;
+`
+export const LeftCol = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    padding: 2rem;
+    font-family: ivyMode, sans-serif;
+    text-transform: uppercase;
+    font-size: 2.5rem;
+    font-weight: 300;
+    em {
+        text-transform: lowercase;
+    }
+`
+export const RightCol = styled.div`
+    border-left: 1px solid var(--black);
+    display: flex;
+    justify-content: flex-end;
+    align-items: flex-end;
+    position: relative;
+    padding: 2rem;
+`
+export const HeroImage = styled.img`
+    position: absolute;
+    width: 60vw;
+    z-index: -1;
+    top: 10rem;
+    right: -5rem;
+`
