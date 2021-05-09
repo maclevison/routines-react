fd65c969293244864ffc9d88873ee8b6746e3dcc
Maclevison Silva <maclevison@gmail.com>
2021-05-09 16:06:10 +0100

    chore: add components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
 import Hero from '../components/Hero'
+import TopBar from '../components/TopBar'
+import Footer from '../components/Footer'
 
 export default function Home() {
     return (
         <>
+            <TopBar />
             <Hero />
+            <Footer />
         </>
     )
 }
